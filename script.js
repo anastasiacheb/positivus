649cdd81fb0e143ba0adde6e45f3855016ce4d23
Anastasia <161876455+anastasiacheb@users.noreply.github.com>
2024-07-27 23:25:57 +0300

    section

--- script.js
+++ script.js
@@ -15,3 +15,20 @@ for (let i = 1; i < navMenuLinks.length; i++) {
         navMenu.classList.add("menu_invisible");
     })
 }
+
+/*accordion*/
+
+let accButtons = document.querySelectorAll(".card-quest__button");
+
+for (let g = 0; g < accButtons.length; g++){
+    accButtons[g].addEventListener("click", function() {
+        this.parentElement.parentElement.classList.toggle("card-quest_open");
+        let accText = this.parentElement.nextElementSibling
+        /*accText.classList.toggle("card-quest__text_invisible");*/
+        if (accText.style.maxHeight) {
+            accText.style.maxHeight = null;
+        } else {
+            accText.style.maxHeight = accText.scrollHeight + "px";
+        }
+    });
+}
